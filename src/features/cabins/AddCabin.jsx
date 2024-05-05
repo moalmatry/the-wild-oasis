01eb29cfd93b1397e53b2c5c68690145a2b95e93
moalmatry <moalmatry2000@gmail.com>
2024-05-05 12:25:08 +0300

    feat: Login Page

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Button from '../../ui/Button';
-import CreateCabinForm from './CreateCabinForm';
-import Modal from '../../ui/Modal';
+import React from "react";
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
 
 // const AddCabin = () => {
 //   const [isOpenModal, setIsOpenModal] = useState(false);
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,18 +1,38 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
 const Heading = styled.h1`
   ${(props) =>
-    props.as === 'h1' &&
+    props.as === "h1" &&
     css`
       font-size: 30px;
       font-weight: 600;
     `}
+
   ${(props) =>
-    props.as === 'h3' &&
+    props.as === "h2" &&
     css`
-      font-size: 10px;
+      font-size: 30px;
       font-weight: 600;
     `}
+
+
+  ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      text-align: center;
+      font-weight: 600;
+    `}
+
+
+
   line-height: 1.4
 `;
 
