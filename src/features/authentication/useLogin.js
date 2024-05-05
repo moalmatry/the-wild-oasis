de457e746750974b8647ece6489618118a270264
moalmatry <moalmatry2000@gmail.com>
2024-05-05 19:50:44 +0300

    fix: login bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,17 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
-import { useNavigate, redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export const useLogin = () => {
   const queryClient = useQueryClient();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { mutate: login, isPending: isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueryData(["user"], user);
-      // to force page to reload we did not use navigate from react dom
-      window.location.replace("/");
+      queryClient.setQueryData(["user"], user.user);
+      // to force page to reload we did not use navigate from react dom (old solution)
+      // window.location.replace("/");
+      navigate("/", { replace: true });
     },
     onError: (error) => {
       console.log("Error", error);
