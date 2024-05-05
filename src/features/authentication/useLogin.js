4c1b0ca40f6220dabec1ef42d8f5955abcf2a9b7
moalmatry <moalmatry2000@gmail.com>
2024-05-05 13:46:08 +0300

    feat: Login auth

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export const useLogin = () => {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      console.log(user);
+      navigate("/dashboard");
+    },
+    onError: (error) => {
+      console.log("Error", error);
+      toast.error("Email or password is incorrect");
+    },
+  });
+
+  return { login, isLoading };
+};
