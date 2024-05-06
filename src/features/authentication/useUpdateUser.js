12fe0b4f25edfc7b51d3ae259a62a1234af0ecb7
moalmatry <moalmatry2000@gmail.com>
2024-05-06 22:26:10 +0300

    feat: update user

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCurrentUser } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isPending: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: ({ user }) => {
+      toast.success("Updated Successfully");
+      queryClient.setQueryData("user", user);
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+    },
+  });
+
+  return { updateUser, isUpdating };
+};
